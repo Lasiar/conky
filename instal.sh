1fab5991350735673d327cdf30f773011cddde8c
Роман Чалый <cerebrum.ch@gmail.com>
2019-03-30 23:54:14 +0700

    feat(install.sh): add configure gmail

--- instal.sh
+++ instal.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
-declare -a golangModule
-golangModule=("./go/src/gmail" "./go/src/network" "./go/src/weather")
-
 runDir=$(pwd)
 conkyRoot=${HOME}/.conky
 conkyBinary=${conkyRoot}/binary
-#if [[ -d ${conkyRoot} ]]; then
-#    rm -rf ${conkyRoot}/*
-#ff
-if [[ -d ${conkyBinary}/binary ]]; then
-    mkdir ${conkyBinary}
-fi
+conkyConfig=${HOME}/.config/conky
+gmailConfig=${conkyConfig}/gmail
 
-for module in ${golangModule[@]}
-do
-    go build -o  ${conkyBinary}/${module##*/}  ${module}
-    echo ${module} compile
-done
+function CheckAndCreateDir() {
+    if [[ ! -d ${conkyBinary} ]]; then
+        mkdir ${conkyBinary}
+    fi 
+    
+    if [[ ! -d ${conkyConfig} ]]; then
+        mkdir ${conkyConfig}
+    fi
+
+    if [[ ! -d ${gmailConfig} ]]; then
+        mkdir ${gmailConfig}
+    fi
+}
+
+function InstallBinary() {
+    declare -a golangModule
+    golangModule=("./go/src/gmail" "./go/src/network" "./go/src/weather")
+
+
+    for module in ${golangModule[@]}
+    do
+        go build -o  ${conkyBinary}/${module##*/}  ${module}
+        echo ${module} compile
+    done
+}
+
+function AddGmailConfigurations() {
+    echo "Please go to \"https://developers.google.com/gmail/api/quickstart/go\" click on  button \"ENABLE THE GMAIL API\""
+    echo "and save download client configuration"
+    echo "enter path gmail client configuration"
+    read pathGmailConf
+
+    mv ${pathGmailConf} ${gmailConfig}
+}
+
+function main() {
+    CheckAndCreateDir
+    AddGmailConfigurations
+    InstallBinary
+}
+
+main
