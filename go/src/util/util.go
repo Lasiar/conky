d409d97ef8a19c55fe431db715ced578c0ce3400
Роман Чалый <cerebrum.ch@gmail.com>
2019-04-01 11:44:08 +0700

    refactor(go/util): mve get home dir separete package

--- go/src/util/util.go
+++ go/src/util/util.go
@@ -1,1 +1,15 @@
 package util
+
+import (
+	"log"
+	"os/user"
+)
+
+// Get home directory current user
+func GetHomeDir() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatalf("cant find user dir %v", err)
+	}
+	return usr.HomeDir
+}
